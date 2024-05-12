1234bb6bee67482fecd55afcfd5838eb858e289b
MuhammadAbdiel <abdielfirdie@gmail.com>
2024-05-12 19:11:37 +0700

    export implemented

---
+++ src/api/exports/index.js
@@ -0,0 +1,18 @@
+const ExportHandler = require("./handler");
+const routes = require("./routes");
+
+module.exports = {
+  name: "exports",
+  version: "1.0.0",
+  register: async (
+    server,
+    { producerService, playlistsService, validator }
+  ) => {
+    const exportHandler = new ExportHandler(
+      producerService,
+      playlistsService,
+      validator
+    );
+    server.route(routes(exportHandler));
+  },
+};
